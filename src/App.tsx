7e4e3bb3f1e4e7092f1d347cc675116f9d8e44d2
shine <xiaoshaoqq@gmail.com>
2019-11-02 22:02:48 +0800

    test prettier

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App: React.FC = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React hhhhh
+          Learn React
         </a>
       </header>
     </div>
