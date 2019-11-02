04fac17319510eb1968494862041d3153ebb1485
shine <xiaoshaoqq@gmail.com>
2019-11-02 23:41:11 +0800

    局部热更新

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,16 @@ import './index.less'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const rootEl = document.getElementById('root')
+
+ReactDOM.render(<App />, rootEl)
+
+if ((module as any).hot) {
+  ;(module as any).hot.accept('./App', () => {
+    const NextApp = require('./App').default
+    ReactDOM.render(<NextApp />, rootEl)
+  })
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
