f9eb06e7e829849a00ae40047238a655a9593d7e
ShineShao <xiaoshaoqq@gmail.com>
2023-03-05 18:43:01 +0800

    update

--- src/page/home/home.tsx
+++ src/page/home/home.tsx
@@ -7,7 +7,7 @@ const Home = () => {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className="_page-home">
       <div>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
@@ -15,20 +15,31 @@ const Home = () => {
         <a href="https://reactjs.org" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a
+          href="https://www.typescriptlang.org/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg"
+            className="logo ts"
+            alt="typescript logo"
+          />
+        </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + Ts</h1>
       <div className="card">
         <button onClick={() => setCount(count => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/home/home.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite、React and Ts logos to learn more
       </p>{' '}
-    </>
+    </div>
   )
 }
 
