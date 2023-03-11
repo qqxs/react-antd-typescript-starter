618d5106055122638370334e132c5d64d9e3d8be
ShineShao <xiaoshaoqq@gmail.com>
2023-03-11 22:35:03 +0800

    feat: add image compress plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteImagemin from 'vite-plugin-imagemin'
 import path from 'path'
 
 import sassVar2JSON from './scripts/sass-to-json'
@@ -52,5 +53,34 @@ export default defineConfig({
     $__THEME__$: theme, // antd theme
     $__SENTRY__$: OPEN_SENTRY // process.env.NODE_ENV === 'production' // open sentry
   },
-  plugins: [react()]
+  plugins: [
+    react(),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false
+      },
+      optipng: {
+        optimizationLevel: 7
+      },
+      mozjpeg: {
+        quality: 20
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4
+      },
+      svgo: {
+        plugins: [
+          {
+            name: 'removeViewBox'
+          },
+          {
+            name: 'removeEmptyAttrs',
+            active: false
+          }
+        ]
+      }
+    })
+  ]
 })
