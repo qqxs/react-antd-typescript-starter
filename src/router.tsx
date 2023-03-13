03845058bee0d56c24f05a6229fee5e8a72f6274
ShineShao <xiaoshaoqq@gmail.com>
2023-03-13 11:49:01 +0800

    rename

--- src/router.tsx
+++ src/router.tsx
@@ -6,16 +6,16 @@ import Auth from '@/components/Auth/auth'
 import Layout from '@/components/Layout'
 // page
 // import Home from '@/page/home/home'
-const Home = React.lazy(async () => await import('@/page/home/home'))
-const Login = React.lazy(async () => await import('@/page/login/login'))
+const Home = React.lazy(async () => await import('@/pages/home/home'))
+const Login = React.lazy(async () => await import('@/pages/login/login'))
 const Register = React.lazy(
-  async () => await import('@/page/register/register')
+  async () => await import('@/pages/register/register')
 )
-const Hello = React.lazy(async () => await import('@/page/hello'))
-const Error404 = React.lazy(async () => await import('@/page/error/404'))
+const Hello = React.lazy(async () => await import('@/pages/hello'))
+const Error404 = React.lazy(async () => await import('@/pages/error/404'))
 
 // ------------------ auth page ---------------------------
-const Profile = React.lazy(async () => await import('@/page/profile/profile'))
+const Profile = React.lazy(async () => await import('@/pages/profile/profile'))
 // ------------------ end auth page -----------------------
 // end page
 
