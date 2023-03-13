1a7ab0983b1e41a1d74a56db60b0a2ba722f282d
ShineShao <xiaoshaoqq@gmail.com>
2023-03-13 12:28:42 +0800

    feat: update 404 content

--- src/pages/error/404.tsx
+++ src/pages/error/404.tsx
@@ -2,7 +2,47 @@ import React from 'react'
 import { classPrefix } from '@/constant'
 
 const NotFound = () => {
-  return <div className={`${classPrefix}_notfound`}>notfound</div>
+  return (
+    <div
+      className={`${classPrefix}_notfound`}
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh'
+      }}
+    >
+      <section
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '40px'
+        }}
+      >
+        <span
+          style={{
+            borderBottom: 0,
+            borderRight: '1px solid #EAEAEA',
+            padding: '0 20px 0 0',
+            width: 'auto'
+          }}
+        >
+          404
+        </span>
+        <p
+          style={{
+            height: '100%',
+            lineHeight: '40px',
+            paddingLeft: '20px',
+            margin: '0'
+          }}
+        >
+          The requested path could not be found
+        </p>
+      </section>
+    </div>
+  )
 }
 
 export default NotFound
