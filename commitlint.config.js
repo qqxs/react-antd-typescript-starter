f8751ea8094598cb2636396e30093a2d2f1a2361
ShineShao <xiaoshaoqq@gmail.com>
2023-03-16 21:34:49 +0800

    feat: commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'doc',
+        'style',
+        'refactor',
+        'test',
+        'revert',
+        'build',
+        'ci',
+        'perf',
+        'conf'
+      ]
+    ]
+  }
+}
