4209fbbfa71fe68d606c2c0fcf92b8d09410bc6b
ShineShao <xiaoshaoqq@gmail.com>
2023-03-25 00:11:46 +0800

    doc: annotation

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,17 +5,17 @@ module.exports = {
       2,
       'always',
       [
-        'feat',
-        'fix',
-        'doc',
-        'style',
-        'refactor',
-        'test',
-        'revert',
-        'build',
-        'ci',
-        'perf',
-        'conf'
+        'feat', // 特性
+        'fix', // 修复
+        'doc', // 文档
+        'style', // 代码格式（不影响功能，例如空格、分号等格式修正）
+        'refactor', // 代码重构（不包括 bug 修复、功能新增）
+        'test', // 测试
+        'revert', // 回退
+        'build', // 构建
+        'ci', // 影响生成系统或外部依赖关系的更改
+        'perf', // 性能优化
+        'conf' // 配置文件修改
       ]
     ]
   }
