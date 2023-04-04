5eb0d15ac9b5d3a1e768bd9c8c71d5c4b52d87fb
ShineShao <xiaoshaoqq@gmail.com>
2023-04-04 21:02:14 +0800

    ci: update eslint-config-xx

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,16 +12,5 @@ module.exports = {
       'vitest.setup.ts',
     ],
   },
-  rules: {
-    // '@typescript-eslint/space-before-function-paren': 'off',
-    // '@typescript-eslint/explicit-function-return-type': 'off',
-    // '@typescript-eslint/strict-boolean-expressions': 'off',
-    // '@typescript-eslint/no-confusing-void-expression': 'off',
-    // '@typescript-eslint/triple-slash-reference': 'off',
-    // '@typescript-eslint/indent': 'off',
-    // '@typescript-eslint/await-thenable': 'off',
-    // 'multiline-ternary': 'off',
-    // '@typescript-eslint/no-unused-vars': 1, // warn
-    // 'react-hooks/exhaustive-deps': 2 // error
-  },
+  rules: {},
 };
