3a7e075106618affd0e8b5f46f4057619a1dd036
ShineShao <xiaoshaoqq@gmail.com>
2023-09-24 22:11:06 +0800

    feat: loading style full screen

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,5 +1,9 @@
 import styles from './loading.module.scss';
 
+/**
+ * @description 页面级别懒加载loading
+ * @returns
+ */
 const Loading = () => <div className={styles.loading}>Loading...</div>;
 
 export default Loading;
