cb3b5b931ffe241b3a5449e311a1f4fcb02a6121
ShineShao <xiaoshaoqq@gmail.com>
2024-10-05 22:24:22 +0800

    feat: update sentry

--- src/sentry.ts
+++ src/sentry.ts
@@ -8,11 +8,27 @@ function InitSentry() {
   Sentry.init({
     // https://develop.sentry.dev/sdk/overview/
     // config your sentry
-    dsn: '__DSN__', // 'https://examplePublicKey@o0.ingest.sentry.io/0'
-    integrations: [new Sentry.BrowserTracing()],
-    // We recommend adjusting this value in production, or using tracesSampler
-    // for finer control
+    // eslint-disable-next-line no-undef
+    dsn: $__SENTRY__DSN__$, // 'https://examplePublicKey@o0.ingest.sentry.io/0'
+    integrations: [
+      Sentry.browserTracingIntegration({
+        // disable automatic span creation
+        instrumentNavigation: false,
+        instrumentPageLoad: false,
+      }),
+    ],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for tracing.
     tracesSampleRate: 1.0,
+
+    // Set `tracePropagationTargets` to control for which URLs trace propagation should be enabled
+    tracePropagationTargets: [/^\//, /^https:\/\/yourserver\.io\/api/],
+
+    // Capture Replay for 10% of all sessions,
+    // plus for 100% of sessions with an error
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
   });
 }
 
